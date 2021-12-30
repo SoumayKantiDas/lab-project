6971f938fe2b9d0dd084599af05e3af86c62decb
Soumay Kanti Das <72224262+SoumayKantiDas@users.noreply.github.com>
2021-12-31 00:06:40 +0930

    Add files via upload

--- views.py
+++ views.py
@@ -1,24 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpRequest
-from django.shortcuts import render
 import requests
-def github(request):
-    search_result = {}
-    if 'username' in request.GET:
-        username = request.GET['username']
-        url = 'https://api.github.com/users/%s' % username
+from bs4 import BeautifulSoup
+
+# Create your views here.
+
+def index(request):
+    repos=False
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        url = 'https://api.github.com/users/%s/repos' % username
         response = requests.get(url)
-        search_was_successful = (response.status_code == 200)  
-        search_result = response.json()
-        search_result['success'] = search_was_successful
-        search_result['rate'] = {
-            'limit': response.headers['X-RateLimit-Limit'],
-            'remaining': response.headers['X-RateLimit-Remaining'],
-        }
-        if 'username' in request.GET:
-            username = request.GET['username']
-            url = 'https://api.github.com/users/%s/repos' % username
-            response = requests.get(url)
-            search_was_successful = (response.status_code == 200)  
-            search_result = response.json()
-    return render(request, 'app/github.html', {'search_result': search_result})
+        repos = response.json()
+    return render(request, 'index.html', context={'repos':repos})
+
+def commit_view(request, user, name, *args, **kwargs):
+    repo_name = name
+    username = user
+    url = 'https://api.github.com/repos/%s/%s/commits' %(username,repo_name)
+    response = requests.get(url)
+    commits = response.json()
+    return render(request, 'commits.html', context={'commits':commits,'repo_name':repo_name,'username':username})
+
+def commit_files_view(request, user, name, ref, *args, **kwargs):
+    username = user
+    repo_name = name
+    refference = ref
+    url = 'https://api.github.com/repos/%s/%s/commits/%s' %(username,repo_name,refference)
+    response = requests.get(url)
+    codes = response.json()
+    return render(request, 'File_name.html', context={'codes':codes,'repo_name':repo_name,'username':username,'refference':refference})
+
+def code_view(request, user, name, ref, file, *args, **kwargs):
+    username = user
+    repo_name = name
+    refference = ref
+    filename = file.replace("+","/")
+    url = "https://github.com/%s/%s/raw/%s/%s" %(username,repo_name,refference,filename)
+    r = requests.get(url)
+    htmlContent = r.content
+    soup = BeautifulSoup(htmlContent, 'html.parser')
+    soup_pretty = soup.prettify()
+    print(soup_pretty)
+    return render(request, 'code.html', context={'soup_pretty':soup_pretty})
